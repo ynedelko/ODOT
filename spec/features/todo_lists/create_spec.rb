d3e8a179f6e3ab2e5ab6120faf8082cfa3246243
Yelena Nedelko <ynedelko@gmail.com>
2015-10-11 12:38:37 -0700

    add presence and length validation to todo list title

---
+++ spec/features/todo_lists/create_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe "Creating todo lists" do
+  it "redirects to the todo list index page on success" do
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New Todo List")
+
+    fill_in "Title", with: "My To Do List"
+    fill_in "Description", with: "This is today's list."
+    click_button "Create Todo list"
+    expect(page).to have_content("My To Do List")
+  end
+
+  it "displays an error when the todo list has no title" do
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New Todo List")
+
+    fill_in "Title", with: ""
+    fill_in "Description", with: "This is today's list."
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("This is today's list.")
+  end
+
+  it "displays an error when the todo list has a title with less than 3 characters" do
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New Todo List")
+
+    fill_in "Title", with: "Hi"
+    fill_in "Description", with: "This is today's list."
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("This is today's list.")
+  end
+end
