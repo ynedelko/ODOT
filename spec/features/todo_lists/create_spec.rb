5f41eb7216e9e43b05b9d93d35a6c590fb0d0bec
Yelena Nedelko <ynedelko@gmail.com>
2015-10-11 12:48:13 -0700

    add length and presence validations for list description

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -47,4 +47,40 @@ describe "Creating todo lists" do
     visit "/todo_lists"
     expect(page).to_not have_content("This is today's list.")
   end
+
+  it "displays an error when the todo list has no description" do
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New Todo List")
+
+    fill_in "Title", with: "Today"
+    fill_in "Description", with: ""
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("Today")
+  end
+
+  it "displays an error when the todo list has a description less than 4 characters" do
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New Todo List")
+
+    fill_in "Title", with: "Today"
+    fill_in "Description", with: "wlk"
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("Today")
+  end
 end
